ecac1edc2b8451b73c0f376c9f09349b43a08769
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-07-21 21:23:18 +0200

    Add P2A source preview lab interface

---
+++ samples/jozz_scan_preview_lab.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2026 Jozz Vehicle contributors
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+struct Sample;
+struct SampleContext;
+
+Sample* CreateJozzScanSourcePreviewLab( SampleContext* context );
